0997ffcb2e6215aee5162de58ff13a4082984354
Nick Noel <nicknoel251289@gmail.com>
2017-09-07 02:58:25 -0400

    event listener added

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,29 +1,42 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-    var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
 
     var revealFirstPoint = function() {
-      points[2].style.opacity = 1;
-      points[2].style.transform = "scaleX(1) translateX(-23)";
-      points[2].style.msTransform = "scaleX(1) translateX(-23)";
-      points[2].style.WebkitTransform = "scaleX(1) translateX(-23)";
+      points[0].style.opacity = 1;
+      points[0].style.transform = "scaleX(1) translateX(3)";
+      points[0].style.msTransform = "scaleX(1) translateX(3)";
+      points[0].style.WebkitTransform = "scaleX(1) translateX(3)";
     };
 
     var revealSecondPoint = function() {
-      points[2].style.opacity = 1;
-      points[2].style.transform = "scaleX(1) translateX(-23)";
-      points[2].style.msTransform = "scaleX(1) translateX(-23)";
-      points[2].style.WebkitTransform = "scaleX(1) translateX(-23)";
+      points[1].style.opacity = 1;
+      points[1].style.transform = "scaleX(1) translateX(3)";
+      points[1].style.msTransform = "scaleX(1) translateX(3)";
+      points[1].style.WebkitTransform = "scaleX(1) translateX(3)";
     };
 
     var revealThirdPoint = function() {
         points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateX(-23)";
-        points[2].style.msTransform = "scaleX(1) translateX(-23)";
-        points[2].style.WebkitTransform = "scaleX(1) translateX(-23)";
+        points[2].style.transform = "scaleX(1) translateX(3)";
+        points[2].style.msTransform = "scaleX(1) translateX(3)";
+        points[2].style.WebkitTransform = "scaleX(1) translateX(3)";
     };
 
     revealFirstPoint();
     revealSecondPoint();
     revealThirdPoint();
 };
+
+window.onload = function() {
+  if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+          animatePoints(pointsArray);
+      }
+   });
+}
